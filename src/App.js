4bd0598303c248aded9fd5d93f7e396f8530b971
madhav langaliya <madhav.langaliya@trootech.com>
2024-02-02 17:28:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 import Education from './components/Education/Education';
-import Certifications from "./components/Certifications/Certifications";
+// import Certifications from "./components/Certifications/Certifications";
 import Projects from "./components/Projects/Project";
 import AboutMe from "./components/About Me/AboutMe";
 import { useSelector } from "react-redux";
--- src/Data/PersonalData.js
+++ src/Data/PersonalData.js
@@ -1,13 +1,13 @@
 const PersonalData = {
     firstName: "Madhav",
     lastName: "Langaliya",
-    nickName:"<  killer - abhi >",
-    mobNo:"+91 8299069282",
-    email:"abhi09148299@gmail.com",
+    nickName:"<  madhav - langaliya >",
+    mobNo:"+91 9909292899",
+    email:"madhavsoni787@gmail.com",
     address:"India",
-    aboutMe:"Hi Everyone, I am Madhav Langaliya from Gujarat, India.I learned C, C++, Python, JavaScript etc, Worked with libraries & frameworks like ReactJS, NodeJS. Have a keen knowledge of databases like MySQL, MongoDB and Firebase. Eager to contribute to innovative projects and gain hands-on experience in a dynamic and collaborative",
+    aboutMe:"Hi Everyone, I am Madhav Langaliya from Gujarat, India.I am a dedicated MEAN stack developer with a proven track record of delivering innovative solutions by leveraging Firebase, PostgreSQL, and MySQL. With a keen eye for detail and a passion for continuous learning, I am committed to staying at the forefront of technology trends and delivering exceptional results in every project I undertake.",
     resumeLink:'https://drive.google.com/file/d/1vfnzDcN4uATaNZ7cc9kdKvep9ZBUN_be/view?usp=sharing',
     imageUrl:"https://drive.usercontent.google.com/download?id=1bAAQPj6lDBsbf-MvHYxNGr9m-p5Yc7uA&export=view",
 }
-export const autoTypeData=['3rd Year IT Grad', 'MERN Stack Developer','Flutter App Developer', 'Open Source Contributor'];
+export const autoTypeData=['MEAN Stack Developer', 'MERN Stack Developer','Full stack Developer', 'Software Engineer'];
 export default PersonalData;
