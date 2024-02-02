171e1b89f0b76cd87d07b66bed303bcbb1182557
madhav langaliya <madhav.langaliya@trootech.com>
2024-02-02 13:56:22 +0530

    initial commit portfolio

---
+++ src/Data/PersonalData.js
@@ -0,0 +1,13 @@
+const PersonalData = {
+    firstName: "Madhav",
+    lastName: "Langaliya",
+    nickName:"<  killer - abhi >",
+    mobNo:"+91 8299069282",
+    email:"abhi09148299@gmail.com",
+    address:"India",
+    aboutMe:"Hi Everyone, I am Madhav Langaliya from Gujarat, India.I learned C, C++, Python, JavaScript etc, Worked with libraries & frameworks like ReactJS, NodeJS. Have a keen knowledge of databases like MySQL, MongoDB and Firebase. Eager to contribute to innovative projects and gain hands-on experience in a dynamic and collaborative",
+    resumeLink:'https://drive.google.com/file/d/1vfnzDcN4uATaNZ7cc9kdKvep9ZBUN_be/view?usp=sharing',
+    imageUrl:"https://drive.usercontent.google.com/download?id=1bAAQPj6lDBsbf-MvHYxNGr9m-p5Yc7uA&export=view",
+}
+export const autoTypeData=['3rd Year IT Grad', 'MERN Stack Developer','Flutter App Developer', 'Open Source Contributor'];
+export default PersonalData;
---
+++ src/Data/SocialData.js
@@ -0,0 +1,16 @@
+const githubLink="https://github.com/killer-abhi";
+const instaLink="https://instagram.com/killer._abhi";
+const linkedInLink="https://in.linkedin.com/in/madhav-langaliya-298ba8139";
+const twitterLink="https://twitter.com/ABHIDK0914";
+const emailLink="mailto:dk9554654249@gmail.com";
+
+const profileLinks={
+    githubLink,
+    instaLink,
+    linkedInLink,
+    twitterLink,
+    emailLink
+}
+const SocialData=profileLinks;
+export default SocialData;
+
